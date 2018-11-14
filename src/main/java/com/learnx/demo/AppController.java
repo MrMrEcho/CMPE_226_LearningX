9c6724924ac2e26a60abff07279e4d9221946b94
Jiaqi Qin <frank.qjq@outlook.com>
2018-11-14 15:58:32 -0800

    add more example of displaying a list of objects

--- src/main/java/com/learnx/demo/AppController.java
+++ src/main/java/com/learnx/demo/AppController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class AppController {
@@ -11,6 +14,11 @@ public class AppController {
     public String index(Model model) {
         System.out.println("first page");
         model.addAttribute("message", "hello from server");
+
+        List<Course> courses = new ArrayList<>();
+        courses.add(new Course("Java", "Jiaqi"));
+        courses.add(new Course("Python", "Tianxiang"));
+        model.addAttribute("courses", courses);
         return "greeting";
     }
 }
---
+++ src/main/java/com/learnx/demo/Course.java
@@ -0,0 +1,12 @@
+package com.learnx.demo;
+
+public class Course {
+
+    public String name;
+    public String teacher;
+
+    public Course(String name, String teacher) {
+        this.name = name;
+        this.teacher = teacher;
+    }
+}
