2c3f692e74e532ed6b12e4c2556f50aa97724f94
Jiaqi Qin <frank.qjq@outlook.com>
2018-11-27 15:07:25 -0800

    add AppUserService.getUserById(long)

--- src/main/java/com/learnx/demo/service/AppUserService.java
+++ src/main/java/com/learnx/demo/service/AppUserService.java
@@ -7,4 +7,6 @@ public interface AppUserService {
     AppUser authenticate(String username, String password) throws Exception;
 
     AppUser Create(String username, String password, AppUser.Role role);
+
+    AppUser getUserById(long userId);
 }
