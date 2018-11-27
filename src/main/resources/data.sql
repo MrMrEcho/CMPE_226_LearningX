70cd109c5460804f84f13bd0aa60bce8ebbd8c99
Jiaqi Qin <frank.qjq@outlook.com>
2018-11-26 20:37:07 -0800

    new schema template to create table and import sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO AppUser (username, password, appRole)
+VALUES ('bofan', '123', 1);
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,193 +1,27 @@
-drop database if exists learingX;
-create database learingX;
-use learingX;
-
 DROP TABLE IF EXISTS AppUser;
 CREATE TABLE AppUser
 (
-    User_name VARCHAR(64) NOT NULL,
-    User_password VARCHAR(128) NOT NULL,
-    User_type VARCHAR(10) NOT NULL,
-    Email VARCHAR(256) NOT NULL,
-    PRIMARY KEY (User_name)
+    id          int auto_increment  primary key,
+    username    varchar(32) NOT NULL,
+    password    varchar(32) NOT NULL,
+    appRole     int
 );
 
-    
-    create table Institute
-    (
-    Institute_id		char(10),
-	Name		varchar(50),
-    primary key (Institute_id)
-    
-    );
-    
-	create table Course
-	(
-    Course_id		char(10),
-    Institute_id		char(10),
-	Name		varchar(50),
-    Rate		int,
-    Section	varchar(20), 
-    primary key (Course_id),
-	foreign key(Institute_id) references Institute(Institute_id)
+create or replace view Student
+as
+select * from AppUser where appRole=0;
 
-    
-	);
-    
-	create table Prequisites(
-		Course_id		char(10),
-        Prequisite		varchar(50),
-        primary key (Course_id),
-		foreign key(Course_id) references Course(Course_id)
-    );
-    
-        create table CourseSeries
-	(
-	CourseSeries_id 	char(10),
-    Institute_id		char(10),
-    Name		varchar(50),
-    primary key (CourseSeries_id),
-	foreign key(Institute_id) references Institute(Institute_id)
+create or replace view Instructor
+as
+select * from AppUser where appRole=1;
 
-	); 
-    
-    create table Contain
-    (
-		Course_id		varchar(10),
-		CourseSeries_id 	char(10),
-        primary key(Course_id, CourseSeries_id),
-		foreign key(Course_id) references Course(Course_id) on delete cascade,
-        foreign key(CourseSeries_id) references CourseSeries(CourseSeries_id) on delete cascade
+create or replace view Institute
+as
+select * from AppUser where appRole=2;
 
-    );
-    
-	create table Instructor
-    (
-    Instructor_id		char(10),
-    Rate		int,
-    Institute_id		char(10),
-    Start_date	date,
-    Name		varchar(50),
-    primary key(Instructor_id),
-	foreign key(Institute_id) references Institute(Institute_id) on delete cascade
+create or replace view AdminUser
+as
+select * from AppUser where appRole=3;
 
-    );
-    
-	create table Taught_by
-    (
-		Instructor_id		char(10),
-        Course_id		char(10),
-        primary key(Instructor_id,Course_id),
-        foreign key(Course_id) references Course(Course_id) on delete cascade,
-		foreign key(Instructor_id) references Instructor(Instructor_id) on delete cascade
-    );
-    
-    
-        create table Material
-    (
-		Course_id		char(10),
-		Material_id		char(10),
-        Instructor_id		char(10),
-        URL		varchar(8000),
-        Type	varchar(20),
-        Content		varchar(8000),
-        primary key(Course_id,Material_id),
-        foreign key(Course_id) references Course(Course_id) on delete cascade,
-		foreign key(Instructor_id) references Instructor(Instructor_id)
 
-        
-    );
-    
-    
-	create table HomeworkExam
-    (
-		Course_id		char(10),
-		Work_id		char(10),
-        Due_date		date,
-		Content			varchar(8000),
 
-        primary key(Work_id,Course_id),
-        
-		foreign key(Course_id) references Course(Course_id) on delete cascade
-        
-    );
- 
-    
-	create table Student
-	(
-    Student_id		char(10),
-	Name		varchar(50),
-    primary key (Student_id)
-	);
-    
-	CREATE TABLE Submission (
-    Work_id		char(10),
-    Student_id CHAR(10),
-    Instructor_id CHAR(10),
-    Grade INT,
-    PRIMARY KEY (Work_id,Student_id , Instructor_id),
-    
-    FOREIGN KEY (work_id)
-        REFERENCES HomeworkExam (work_id)
-        ON DELETE CASCADE,
-        
-    FOREIGN KEY (Student_id)
-        REFERENCES Student (Student_id)
-        ON DELETE CASCADE,
-        
-    FOREIGN KEY (Instructor_id)
-        REFERENCES Instructor (Instructor_id)
-        ON DELETE CASCADE
-);
-
-    
-create table Enroll(
-    
-    Course_id		char(10),
-    Student_id		char(10),
-    Rating		int,
-
-    primary key(Course_id,Student_id),
-	foreign key(Course_id) references Course(Course_id) on delete cascade,
-    foreign key(Student_id) references Student(Student_id) on delete cascade
-    );
-    
-    
-create table Question
-	(
-    
-    Question_id		char(10),
-    Course_id		char(10),
-
-	primary key (Course_id,Question_id),
-	foreign key(Course_id) references Course(Course_id) on delete cascade
-
-	);
-    
-    
-    create table Question_StudentAsk
-    (
-     Course_id		char(10),
-	 Student_id		char(10),
-	 Question_id		char(10),
-
-     Title		varchar(50),
-	 Content		varchar(280),
-	 Post_date		date,
-     primary key (Course_id,Student_id,Question_id),
-	 foreign key(Course_id,Question_id) references Question(Course_id,Question_id) on delete cascade,
-	 foreign key(Student_id) references Student(Student_id) on delete cascade
-    );
-    
-	create table Question_InstructorReply
-    (
-     Question_id		char(10),
-     Course_id		char(10),
-     Instructor_id	char(10),
-     Title		varchar(50),
-	 Reply		varchar(840),
-	 Reply_date		date,
-     primary key (Course_id,Instructor_id,Question_id),
-	 foreign key(Course_id,Question_id) references Question(Course_id,Question_id) on delete cascade,
-	 foreign key(Instructor_id) references Instructor(Instructor_id) on delete cascade
-    );
