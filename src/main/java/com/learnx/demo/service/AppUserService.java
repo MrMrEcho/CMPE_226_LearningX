c0e3b486bfd218dfa1cdb7aa5030379c71d593f4
Jiaqi Qin <frank.qjq@outlook.com>
2018-11-27 15:36:28 -0800

    add AppUserService.listInstructorsByInstituteId

--- src/main/java/com/learnx/demo/service/AppUserService.java
+++ src/main/java/com/learnx/demo/service/AppUserService.java
@@ -2,6 +2,8 @@ package com.learnx.demo.service;
 
 import com.learnx.demo.model.AppUser;
 
+import java.util.List;
+
 public interface AppUserService {
 
     AppUser authenticate(String username, String password) throws Exception;
@@ -9,4 +11,6 @@ public interface AppUserService {
     AppUser Create(String username, String password, AppUser.Role role);
 
     AppUser getUserById(long userId);
+
+    List<AppUser> listInstructorsByInstituteId(long instituteId);
 }
