1aa3e65c95e95f04515692ec14b64cd76840df9f
JuYunCheng <juyun.cheng@sjsu.edu>
2018-11-26 22:51:23 -0800

    Convert to new model

--- src/main/java/com/learnx/demo/controller/AppController.java
+++ src/main/java/com/learnx/demo/controller/AppController.java
@@ -1,7 +1,7 @@
 package com.learnx.demo.controller;
 
 import com.learnx.demo.model.*;
-import com.learnx.demo.service.UserService;
+import com.learnx.demo.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,11 +17,21 @@ import java.util.List;
 public class AppController {
 
     @Autowired
-    UserService userService;
+    AppUserService appuserService;
+
+    @Autowired
+    SeriesService seriesService;
+
+    @Autowired
+    CourseService courseService;
+
+    @Autowired
+    RatingService rateService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public ModelAndView showIndex(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView showIndex(HttpServletRequest request) {
         ModelAndView mav = new ModelAndView("index");
+        mav.addObject("series", seriesService.listSeries());
         return mav;
     }
 
@@ -32,29 +40,30 @@ public class AppController {
     //  ======================
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public ModelAndView showLogin(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView showLogin() {
         ModelAndView mav = new ModelAndView("login");
-        mav.addObject("login", new User());
+        mav.addObject("login");
         return mav;
     }
 
-    @RequestMapping(value = "/loginProcess", method = RequestMethod.POST)
-    public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response,
-                                     @ModelAttribute("login") Login login) {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView loginProcess(HttpServletRequest request) {
         ModelAndView mav = null;
-        User user = userService.validate(login);
-        if(user == null){
-            mav = new ModelAndView("login");
-            mav.addObject("message", "Username or Password is wrong!!");
-        } else{
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        try{
+            AppUser user = appuserService.authenticate(username, password);
             mav = new ModelAndView("welcome");
-            mav.addObject("username", login.getUsername());
+            mav.addObject("username", user.getUsername());
+        } catch(Exception e){
+            mav.addObject("message", e.getMessage());
+            mav = new ModelAndView("login");
         }
         return mav;
     }
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
-    public ModelAndView showLogout(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView showLogout(HttpServletRequest request) {
         ModelAndView mav = new ModelAndView("logout");
         return mav;
     }
@@ -65,20 +74,26 @@ public class AppController {
     //  ======================
 
     @RequestMapping(value = "/signup", method = RequestMethod.GET)
-    public ModelAndView showRegister(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView showRegister(HttpServletRequest request) {
         ModelAndView mav = new ModelAndView("register");
         mav.addObject("user", new User());
         return mav;
     }
 
-    @RequestMapping(value = "/registerProcess", method = RequestMethod.POST)
-    public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response,
-                                @ModelAttribute("user") User user) {
-        User newUser = userService.create(user);
-        if(newUser == null){
-            throw new RuntimeException("error when signup");
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView addUser(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        AppUser.Role role = AppUser.Role.valueOf(request.getParameter("type"));
+        ModelAndView mav = null;
+        try {
+            AppUser newUser = appuserService.Create(username, password, role);
+            mav = new ModelAndView("login");
+        } catch (Exception e) {
+            mav.addObject("message", e.getMessage());
+            mav = new ModelAndView("register");
         }
-        return new ModelAndView("welcome", "username", newUser.getUsername());
+        return mav;
     }
 
 
@@ -87,11 +102,9 @@ public class AppController {
     //  ======================
 
     @RequestMapping(value = "/search", method = RequestMethod.POST)
-    public ModelAndView search(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView search(HttpServletRequest request) {
         ModelAndView mav = new ModelAndView("search_result");
-        List<Course> list = new ArrayList<>();
-        list.add(new Course(1232, "2019 Fall", "HTML 5 for beginner", "good!"));
-        list.add(new Course(4543, "2019 Spring", "Java Spring", "good!"));
+        List<Course> list = courseService.searchCourses(request.getParameter("keyword"));
         mav.addObject("courselist", list);
         return mav;
     }
@@ -101,15 +114,15 @@ public class AppController {
     //        courses
     //  ======================
 
-    @RequestMapping(value = "/courses", method = RequestMethod.GET)
-    public ModelAndView courses(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(value = "/courses", method = RequestMethod.POST)
+    public ModelAndView courses(HttpServletRequest request) {
+        Long courseId = Long.valueOf(request.getParameter("courseId"));
         ModelAndView mav = new ModelAndView("single_course");
-        Course c = new Course(1232, "2019 Fall", "HTML 5 for beginner", "good!");
-        mav.addObject("course", c);
-        List<Question> list = new ArrayList<>();
-        list.add(new Question("Hi!", "HiHi", "2019-04-03"));
-        list.add(new Question("Yo!", "YoYo", "2019-03-03"));
-        mav.addObject("questionlist", list);
+        Course course = courseService.getCourseById(courseId);
+        List<Discussion> discussionList = course.getDiscussions();
+        mav.addObject("title", course.getTitle());
+        mav.addObject("instructor", course.getInstructor());
+        mav.addObject("discussionList", discussionList);
         return mav;
     }
 
@@ -117,15 +130,15 @@ public class AppController {
     //        reviews
     //  ======================
 
-    @RequestMapping(value = "/reviews", method = RequestMethod.GET)
-    public ModelAndView getreviews(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(value = "/reviews", method = RequestMethod.POST)
+    public ModelAndView getreviews(HttpServletRequest request) {
+        Long courseId = Long.valueOf(request.getParameter("courseId"));
         ModelAndView mav = new ModelAndView("course_review");
-        List<Rate> list = new ArrayList<>();
-        list.add(new Rate((int) Math.round(3.4 * 20), 3.4, "2019-04-03", "Tom"));
-        list.add(new Rate((int) Math.round(4.5 * 20), 4.5, "2019-03-03", "Mary"));
-        Course c = new Course(1232, "2019 Fall", "HTML 5 for beginner", "good!");
-        mav.addObject("reviewlist", list);
-        mav.addObject("course", c);
+        Course course = courseService.getCourseById(courseId);
+        List<Rating> rateList =  rateService.listRatingsByCourseId(courseId);
+        mav.addObject("rateList", rateList);
+        mav.addObject("title", course.getTitle());
+        mav.addObject("instructor", course.getInstructor());
         return mav;
     }
 
@@ -133,15 +146,15 @@ public class AppController {
     //        homework
     //  ======================
 
-    @RequestMapping(value = "/homework", method = RequestMethod.GET)
-    public ModelAndView gethomework(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(value = "/homework", method = RequestMethod.POST)
+    public ModelAndView gethomework(HttpServletRequest request) {
+        Long courseId = Long.valueOf(request.getParameter("courseId"));
         ModelAndView mav = new ModelAndView("course_homework");
-        List<Homework> list = new ArrayList<>();
-        list.add(new Homework("Project1", "2019-04-03"));
-        list.add(new Homework("Project2", "2019-03-03"));
-        Course c = new Course(1232, "2019 Fall", "HTML 5 for beginner", "good!");
-        mav.addObject("homeworklist", list);
-        mav.addObject("course", c);
+        Course course = courseService.getCourseById(courseId);
+        List<Homework> homeworkList = course.getHomeworks();
+        mav.addObject("homeworkList", homeworkList);
+        mav.addObject("title", course.getTitle());
+        mav.addObject("instructor", course.getInstructor());
         return mav;
     }
 }
