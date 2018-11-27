b097cfda4acac99c3c9a477455f5d1faa9fb0f19
Shikai Jin <jinshikai@gmail.com>
2018-11-26 22:14:26 -0800

    Add rating

---
+++ src/main/java/com/learnx/demo/model/Rating.java
@@ -0,0 +1,16 @@
+package com.learnx.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rating {
+    private Course course;
+    private AppUser student;
+    private int rating;
+}
