406d6d74026521ee88fc92cfb38126f1da4540f3
Jiaqi Qin <frank.qjq@outlook.com>
2018-11-30 12:58:30 -0800

    done add material

--- src/main/java/com/learnx/demo/repository/MaterialRepository.java
+++ src/main/java/com/learnx/demo/repository/MaterialRepository.java
@@ -1,9 +1,12 @@
 package com.learnx.demo.repository;
 
+import com.learnx.demo.entity.Homework;
 import com.learnx.demo.entity.Material;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -25,5 +28,28 @@ public class MaterialRepository {
         Query query = em.createNativeQuery(sql, Material.class).setParameter("courseId", courseId);
 
         return RepositoryUtil.castAll(query.getResultList(), Material.class);
+    }
+
+    @Transactional
+    public Material save(Material toSave) {
+        String sql =
+                "INSERT INTO Material (courseId, title, url) "
+                        + "VALUES (:courseId, :title, :url)";
+        Query query =
+                em.createNativeQuery(sql)
+                        .setParameter("courseId", toSave.getCourseId())
+                        .setParameter("title", toSave.getTitle())
+                        .setParameter("url", toSave.getUrl());
+
+        if (query.executeUpdate() == 0) {
+            return null;
+        }
+
+        Material saved = new Material();
+        saved.setId(RepositoryUtil.getLastInsertId(em));
+        saved.setCourseId(toSave.getCourseId());
+        saved.setTitle(toSave.getTitle());
+        saved.setUrl(toSave.getUrl());
+        return saved;
     }
 }
