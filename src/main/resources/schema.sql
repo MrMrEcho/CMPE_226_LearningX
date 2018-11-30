e8a8468acf0393d80d275c70cee8aa1393f33eef
Tsung Min Huang <tsungmin.huang@sjsu.edu>
2018-11-30 02:02:43 -0800

    reformat sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,11 +14,11 @@ drop table if exists AppUser;$
 
 create table AppUser
 (
-    id          int auto_increment,
-    username    varchar(32) NOT NULL UNIQUE,
-    password    varchar(512) NOT NULL,
-    appRole     int,
-    
+    id       int auto_increment,
+    username varchar(32)  NOT NULL UNIQUE,
+    password varchar(512) NOT NULL,
+    appRole  int,
+
     primary key (id),
 
     fulltext index user_name (username)
@@ -26,14 +26,14 @@ create table AppUser
 
 create table Course
 (
-    id              int auto_increment,
-    title           varchar(128) not null unique,
-    description     varchar(1024),
-    instructorId    int,
-    
+    id           int auto_increment,
+    title        varchar(128) not null unique,
+    description  varchar(1024),
+    instructorId int,
+
     primary key (id),
-    
-    foreign key (instructorId) references AppUser(id)
+
+    foreign key (instructorId) references AppUser (id)
         on delete cascade,
 
     fulltext index course_content (title, description),
@@ -43,14 +43,14 @@ create table Course
 
 create table WorkFor
 (
-    instructorId    int,
-    instituteId     int,
-    
+    instructorId int,
+    instituteId  int,
+
     primary key (instructorId, instituteId),
-    
-    foreign key (instructorId) references AppUser(id)
+
+    foreign key (instructorId) references AppUser (id)
         on delete cascade,
-    foreign key (instituteId) references AppUser(id)
+    foreign key (instituteId) references AppUser (id)
         on delete cascade
 );$
 
@@ -59,24 +59,25 @@ create table Series
     id          int auto_increment,
     instituteId int,
     title       varchar(128) not null,
-    
+    description varchar(1024),
+
     primary key (id),
-    
-    foreign key (instituteId) references AppUser(id)
+
+    foreign key (instituteId) references AppUser (id)
         on delete cascade
 );$
 
 create table CourseSeries
 (
-    courseId    int not null,
-    seriesId    int not null,
-    
+    courseId int not null,
+    seriesId int not null,
+
     primary key (courseId, seriesId),
-    
-    foreign key (courseId) references Course(id)
+
+    foreign key (courseId) references Course (id)
         on delete cascade,
-    
-    foreign key (seriesId) references Series(id)
+
+    foreign key (seriesId) references Series (id)
         on delete cascade
 );$
 
@@ -85,123 +86,132 @@ create table Enroll
     studentId   int not null,
     courseId    int not null,
     isCompleted boolean default false,
-    isDropped boolean default false,
-    
+    isDropped   boolean default false,
+
     primary key (studentId, courseId),
-    
-    foreign key (studentId) references AppUser(id)
+
+    foreign key (studentId) references AppUser (id)
         on delete cascade,
-    
-    foreign key (courseId) references Course(id)
+
+    foreign key (courseId) references Course (id)
         on delete cascade
 );$
 
 create table Rating
 (
-    studentId   int,
-    courseId    int,
-    rating      int,
-    
+    studentId int,
+    courseId  int,
+    rating    int,
+
     primary key (studentId, courseId),
-    
-    foreign key (studentId) references AppUser(id)
+
+    foreign key (studentId) references AppUser (id)
         on delete cascade,
-    
-    foreign key (courseId) references Course(id)
+
+    foreign key (courseId) references Course (id)
         on delete cascade
 );$
 
 create table Material
 (
-    id          int auto_increment,
-    courseId    int not null,
-    title       varchar(256) not null,
-    url         varchar(256),
-    
+    id       int auto_increment,
+    courseId int          not null,
+    title    varchar(128) not null,
+    url      varchar(1024),
+
     primary key (id),
-    
-    foreign key (courseId) references Course(id)
+
+    foreign key (courseId) references Course (id)
         on delete cascade
 );$
 
 create table Homework
 (
-    id          int auto_increment,
-    courseId    int not null,
-    title       varchar(256) not null,
-    content     varchar(256),
-    type        int not null,
-    
+    id       int auto_increment,
+    courseId int          not null,
+    title    varchar(128) not null,
+    content  varchar(256),
+    type     int          not null,
+
     primary key (id),
-    
-    foreign key (courseId) references Course(id)
+
+    foreign key (courseId) references Course (id)
         on delete cascade
 );$
 
 create table Submission
 (
-  studentId  int not null,
-  homeworkId int not null,
-  answer     varchar(256),
-  grade      int     default 0,
-  hasGrade   boolean default false,
+    studentId  int not null,
+    homeworkId int not null,
+    answer     varchar(1024),
+    grade      int     default 0,
+    hasGrade   boolean default false,
 
-  primary key (studentId, homeworkId),
+    primary key (studentId, homeworkId),
 
-  foreign key (studentId) references AppUser(id)
+    foreign key (studentId) references AppUser (id)
         on delete cascade,
 
-  foreign key (homeworkId) references Homework(id)
+    foreign key (homeworkId) references Homework (id)
         on delete cascade
 );$
 
 create table Discussion
 (
-    id          int auto_increment,
+    id       int auto_increment,
     -- userId      int,
     -- courseId    int,
-    userId      int not null,
-    courseId    int not null,
-    title       varchar(128) not null,
-    content     varchar(256),
-    
+    userId   int          not null,
+    courseId int          not null,
+    title    varchar(128) not null,
+    content  varchar(1024),
+
     -- primary key (userId, courseId),
     primary key (id),
-    
+
     -- FIXME: remove user discussion still can keep
-    foreign key (userId) references AppUser(id)
+    foreign key (userId) references AppUser (id)
         on delete cascade,
-    
-    foreign key (courseId) references Course(id)
+
+    foreign key (courseId) references Course (id)
         on delete cascade
 );$
 
 -- Create Views
 create or replace view Student
 as
-select * from AppUser where appRole=0;$
+select *
+from AppUser
+where appRole = 0;$
 
 create or replace view Instructor
 as
-select * from AppUser where appRole=1;$
+select *
+from AppUser
+where appRole = 1;$
 
 create or replace view Institute
 as
-select * from AppUser where appRole=2;$
+select *
+from AppUser
+where appRole = 2;$
 
 create or replace view AdminUser
 as
-select * from AppUser where appRole=3;$
+select *
+from AppUser
+where appRole = 3;$
 
 create or replace view AverageRating
 as
 select C.id as id, C.title as title, avg(R.rating) as rating, count(distinct R.studentId) as count
-from Course C join Rating R on C.id = R.courseId
+from Course C
+         join Rating R on C.id = R.courseId
 group by C.id;$
 
 -- Create stored procedures
 drop function if exists isExam ;$
-create function `isExam` (homeworkId int)
+create function `isExam`(homeworkId int)
     returns boolean
     READS SQL DATA
     DETERMINISTIC
@@ -217,7 +227,7 @@ end ;$
 
 
 drop function if exists getCourseIdByHomeworkId ;$
-create function `getCourseIdByHomeworkId` (homeworkId int)
+create function `getCourseIdByHomeworkId`(homeworkId int)
     returns int
     READS SQL DATA
     DETERMINISTIC
@@ -229,18 +239,20 @@ end ;$
 
 
 drop procedure if exists completeEnroll ;$
-create procedure `completeEnroll` (studentId int, cid int)
+create procedure `completeEnroll`(studentId int, cid int)
 begin
     update Enroll E
     set isCompleted = true
-    where E.studentId = studentId and E.courseId = cid;
+    where E.studentId = studentId
+      and E.courseId = cid;
 end ;$
 
 -- Create triggers
 
 drop trigger if exists AfterSubmissionGraded ;$
 create trigger `AfterSubmissionGraded`
-    after update on Submission
+    after update
+    on Submission
     for each row
 begin
     declare cid int;
@@ -250,5 +262,5 @@ begin
     end if;
 end ;$
 
--- delimiter ;
+delimiter ;
 
